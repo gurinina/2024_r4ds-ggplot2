6c7fbe9811ff64b7b512518dec629a101b8ea74d
gurinina <g.giaever@ubc.ca>
2024-11-14 11:15:58 -0800

    knit gitbook

--- knit_to_gitbook.R
+++ knit_to_gitbook.R
@@ -1,5 +1,18 @@
-# Render the book in GitBook format
+# The  knit_to_gitbook.R script uses the bookdown  package to render the book in GitBook format.
+
+# To run the  knit_to_gitbook.R  script, click on the Source  button at the top of the window.
+
+# load the bookdown package
+library(bookdown)
+
+# removes all files from the _book  directory
+clean_book(clean = TRUE)
+
+# renders the book in GitBook format
 bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
-# Open the resulting GitBook in the Viewer pane
+# opens the GitBook in your browser
 # rstudioapi::viewer("_book/index.html")
+
+# To see the book in your Viewer pane, open 'index.Rmd' in your main directory and click the 'Knit' button.
+
